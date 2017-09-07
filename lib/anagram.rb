0f95fb07ba20c31967886e74fda967522f12e596
annabush092 <annabush092@gmail.com>
2017-09-07 16:26:48 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    #return all anagrams of @word
+    word_array.select do |w|
+      w.split("").sort == word.split("").sort
+    end
+    #if none, return empty
+  end
+end
